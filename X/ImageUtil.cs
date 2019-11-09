6de561e4683c9aa0b7a5cb954958e9e343074ca0
keymoon <kymn0116@gmail.com>
2019-11-10 04:34:32 +0900

    add imageutil

---
+++ X/ImageUtil.cs
@@ -0,0 +1,19 @@
+﻿#if DEBUG
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+public class ImageUtil
+{
+    public static Bitmap ConvertToBitMap(Image image)
+    {
+        Bitmap bitmap = new Bitmap(image.W, image.H);
+        for (int i = 0; i < image.H; i++)
+            for (int j = 0; j < image.W; j++)
+                bitmap.SetPixel(j, i, image[i, j] ? Color.Black : Color.White);
+        return bitmap;
+    }
+}
+#endif
