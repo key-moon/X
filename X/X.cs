5109389fadd66e5f8675a312d00efbb42fcf16c1
keymoon <kymn0116@gmail.com>
2019-11-10 03:54:57 +0900

    [add] image class and solver class

--- X/X.cs
+++ X/X.cs
@@ -9,10 +9,39 @@ public class X
         int[] hw = Console.ReadLine().Split().Select(int.Parse).ToArray();
         int h = hw[0];
         int w = hw[1];
-        bool[][] picture = Enumerable.Repeat(0, h).Select(_ => Console.ReadLine().Select(x => x == '#').ToArray()).ToArray();
-        //solve
-        int res = 0;
+        bool[][] pixels = Enumerable.Repeat(0, h).Select(_ => Console.ReadLine().Select(x => x == '#').ToArray()).ToArray();
+        Image image = new Image(h, w, pixels);
+        int res = new Solver().Solve(image);
         Console.WriteLine(res);
     }
 }
 
+public class Image
+{
+    public readonly int H;
+    public readonly int W;
+    bool[] pixels;
+    public Image(int h, int w, bool[][] pixels)
+    {
+        H = h;
+        W = w;
+        this.pixels = new bool[H * W];
+        int ptr = 0;
+        for (int i = 0; i < H; i++)
+            for (int j = 0; j < W; j++)
+                this.pixels[ptr++] = pixels[i][j];
+    }
+    public bool this[int y, int x]
+    {
+        get { return pixels[y * H + x]; }
+        set { pixels[y * H + x] = value; }
+    }
+}
+
+public class Solver
+{
+    public int Solve(Image image)
+    {
+
+    }
+}
