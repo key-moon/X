bb70dd432ccdaf52a8b0b8a30d166ffb2ecce63e
keymoon <kymn0116@gmail.com>
2019-11-10 07:24:29 +0900

    add joinhorizontal

--- X/ImageUtil.cs
+++ X/ImageUtil.cs
@@ -1,12 +1,33 @@
 ﻿#if DEBUG
 using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
 
 public static class ImageUtil
 {
+    public static Bitmap JoinHorizontal(this Image[] images, int mergin)
+    {
+        var h = images.Max(x => x.H);
+        var w = images.Sum(x => x.W) + mergin * (images.Length - 1);
+        var bitmap = CreateFilledBitmap(h, w, Color.Gray);
+
+        int xOffset = 0;
+        int yOffset = 0;
+        foreach (var image in images)
+        {
+            yOffset = (h - image.H) / 2;
+
+            for (int y = 0; y < image.H; y++)
+                for (int x = 0; x < image.W; x++)
+                    if (image[y, x]) bitmap.SetPixel(xOffset + x, yOffset + y, Color.Black);
+
+            xOffset += image.W + mergin;
+        }
+        return bitmap;
+    }
     public static void Save(this Image image, string name)
     {
         using (Bitmap bmp = ConvertToBitMap(image))
@@ -20,6 +41,14 @@ public static class ImageUtil
         for (int i = 0; i < image.H; i++)
             for (int j = 0; j < image.W; j++)
                 bitmap.SetPixel(j, i, image[i, j] ? Color.Black : Color.White);
+        return bitmap;
+    }
+    public static Bitmap CreateFilledBitmap(int h, int w, Color color)
+    {
+        var bitmap = new Bitmap(w, h);
+        for (int x = 0; x < w; x++)
+            for (int y = 0; y < h; y++)
+                bitmap.SetPixel(x, y, color);
         return bitmap;
     }
 }
