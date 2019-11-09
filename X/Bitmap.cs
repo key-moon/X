269559f7515f72ed96a7fd3b63747456362f2f29
keymoon <kymn0116@gmail.com>
2019-11-10 04:34:18 +0900

    add mock implementation to bitmap

--- X/Bitmap.cs
+++ X/Bitmap.cs
@@ -1,6 +1,13 @@
 ﻿#if !DEBUG
+public enum Color
+{
+    Black,
+    White
+}
 public class Bitmap
 {
+    public Bitmap(int x, int y) { }
+    public void SetPixel(int x, int y, Color color) { }
     public void Save(string path) { }
 }
 #endif
