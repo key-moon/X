0c6a039e9e264c208c18f8065ea6955207580035
keymoon <kymn0116@gmail.com>
2019-11-10 03:46:35 +0900

    write outline

--- X/X.cs
+++ X/X.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 public class X
 {
     public static void Main()
     {
-
+        int testCaseNum = int.Parse(Console.ReadLine());
+        int[] hw = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int h = hw[0];
+        int w = hw[1];
+        bool[][] picture = Enumerable.Repeat(0, h).Select(_ => Console.ReadLine().Select(x => x == '#').ToArray()).ToArray();
+        //solve
+        int res = 0;
+        Console.WriteLine(res);
     }
 }
+
